fd2bf31acef1e7414e85a87ce81d2d3939f78c31
Gustavo Rodrigues <gustavosoftex@gmail.com>
2026-09-25 14:45:10 -0300

    atualizado a url do render

--- app/admin/cardapio/page.tsx
+++ app/admin/cardapio/page.tsx
@@ -21,7 +21,7 @@ export default function CardapioAdmin() {
     async function carregarProdutos() {
         try {
             const response = await fetch(
-                `${process.env.API_URL}/produtos`
+                `${process.env.API_URL}/admin/cardapio/produtos`
             )
 
             if (!response.ok) {
