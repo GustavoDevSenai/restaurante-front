248d867f8ea313d83c3d71257f02672701ad36cf
Gustavo Rodrigues <gustavosoftex@gmail.com>
2026-09-25 15:59:39 -0300

    atualizado a url do render

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -11,13 +11,13 @@ export default function AdminPage() {
     const [preco, setPreco] = useState("")
     const [imagem, setImagem] = useState("")
 
-    async function cadastrarLanche() {
+    async function cadastrarLanche(e) {
 
         e.preventDefault()
 
         try {
 
-            const response = await fetch(`${process.env.API_URL}/produtos`, {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/produtos`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
