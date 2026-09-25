97e3d26f1c99e9945272c2867e61146e17bb65c0
Gustavo Rodrigues <gustavosoftex@gmail.com>
2026-09-25 16:31:16 -0300

    navbar no layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     >
       <body className="min-h-full flex flex-col">
         
-        
+
+
+        <Navbar />
         {children}
         
         </body>
